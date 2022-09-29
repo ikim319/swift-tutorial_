1c81d7ef61fa16069c5fa8363b036cfbca87c29c
Isaac <ikim319@gatech.edu>
2022-09-29 18:35:27 -0400

    Initial Commit

---
+++ swift-tutorial_/swift_tutorial_App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorial_App.swift
+//  swift-tutorial_
+//
+//  Created by Isaac Kim on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorial_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
